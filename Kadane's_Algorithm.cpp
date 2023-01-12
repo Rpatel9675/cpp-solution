c315e06e384db452e9bf6c8da8677557df869cd6
Rocky <pprl735757@gmail.com>
2023-01-12 15:57:31 +0530

    Rat in a Maze problem -1

---
+++ Kadane's_Algorithm.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+     int mx=INT_MIN;
+     int sum=0;
+     for(int i=0;i<n;i++){
+         sum+=arr[i];
+         if(sum>mx){
+             mx=sum;
+         }
+         if(sum<0){
+             sum=0;
+         }
+     }
+     return mx;
+
+
+        // Your code here
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
